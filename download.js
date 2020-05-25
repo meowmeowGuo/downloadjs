994d505513485ae947d8332f39f4c9f3b6db8d64
miaomiaoG <15827061459@163.com>
2020-05-25 15:57:03 +0800

    add demo

--- download.js
+++ download.js
@@ -6,31 +6,28 @@
  * @param {string} url : 下载链接
  * @param {object} [data] : 下载参数
  */
-function getMethod(url, data){
-  let queryString='';
-  if(data){
-    queryString = Objec.entries(data).reduce((res, cur)=>{
-      const {key, value} = cur;
+function getMethod(url, data, downloadName = '') {
+  let queryString = '';
+  if (data) {
+    queryString = Objec.entries(data).reduce((res, cur) => {
+      const { key, value } = cur;
       return res += `${key}=${value}&`;
-    },'?')
+    }, '?')
   }
   const aDom = document.createElement('a');
   aDom.style.display = 'none';
-  if (downloadName) {
-    aDom.download = downloadName;
-  }
   aDom.href = url + queryString;
   document.body.appendChild(aDom);
   aDom.click();
   document.body.removeChild(aDom);
-} 
+}
 
 
 /**
  * @param {string} url : 下载链接
  * @param {object} [data] : 下载参数
  */
-function postMethod(url, data){
+function postMethod(url, data) {
   /**
    * @param {string} name :创建form input的需要的name，最后formdata的key
    * @param {*} value :创建form input的需要的value，最后formdata的value
@@ -72,37 +69,37 @@ function postMethod(url, data){
   document.body.removeChild(downloadForm);// 移除表单
 }
 
- /** 
-   * @param {string} url: 下载的链接
-   * @param {object} [options={}] : 下载配置
-   * @param {string} [options.method='GET'] : 下载方法，get,post
-   * @param {object} [options.data] : 下载时的请求参数
-   * @memberof Download
-   */
-function Download(url, options ={}){
- 
-  if(!url){
+/** 
+  * @param {string} url: 下载的链接
+  * @param {object} [options={}] : 下载配置
+  * @param {string} [options.method='GET'] : 下载方法，get,post
+  * @param {object} [options.data] : 下载时的请求参数
+  * @memberof Download
+  */
+function Download(url, options = {}) {
+
+  if (!url) {
     throw new Error('download need url');
   }
-  const {method = 'GET', data = null} = options;
-  if(!['POST','GET'].includes(method.toLocaleUpperCase)){
+  const { method = 'GET', data = null } = options;
+  if (!['POST', 'GET'].includes(method.toLocaleUpperCase())) {
     /* 只支持get 和 post方式 */
     throw new Error('downloadjs only support get and post');
   }
 
-  if(data && !data.constructor instanceof Object){
+  if (data && !data.constructor instanceof Object) {
     throw new Error(`download data is expected to be object, but got ${data.constructor}`);
   }
 
-  switch(method.toLocaleUpperCase){
+  switch (method.toLocaleUpperCase()) {
     case 'GET':
       getMethod(url, data);
       break;
     case 'POST':
       postMethod(url, data);
       break;
-    default:break;
+    default: break;
   }
 }
 
-exports.Download = Download
+// exports.Download = Download
